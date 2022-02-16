4bb04b9e1cb60526077616a9d6bab397bac20d6d
terjeee <96795863+terjeee@users.noreply.github.com>
2022-02-16 01:20:10 +0100

    addet JS

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const modal = document.querySelector(`.modal`);
+const overlayModal = document.querySelector(`.overlay`);
+const btnCloseModal = document.querySelector(`.close-modal`);
+const btnsOpenModal = document.querySelectorAll(`.show-modal`);
+
+function openModal() {
+  modal.classList.remove(`hidden`);
+  overlayModal.classList.remove(`hidden`);
+}
+function closeModal() {
+  modal.classList.add(`hidden`);
+  overlayModal.classList.add(`hidden`);
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener(`click`, openModal); // open  Modal
+}
+
+btnCloseModal.addEventListener(`click`, closeModal); // close Modal
+overlayModal.addEventListener(`click`, closeModal);
+document.addEventListener(`keydown`, function (event) {
+  if (event.key === `Escape` && !modal.classList.contains(`hidden`)) {
+    closeModal();
+  }
+});
